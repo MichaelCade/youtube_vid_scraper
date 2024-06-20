46eb16415583c81c88d8d8a6ce2fecd6e22d225d
MichaelCade <michael.cade@outlook.com>
2024-06-20 19:38:20 +0100

    comment out custom commands in bloggy.sh

--- bloggy.sh
+++ bloggy.sh
@@ -18,13 +18,13 @@ echo "$videos" | while read -r video; do
   md_file="day${day}.md"
   
   # Insert video title, thumbnail, and clickable URL at the top of the markdown file
-  echo "# $title" > "$md_file"
-  echo "[![Watch the video](thumbnails/day${day}.png)]($url)" >> "$md_file"
-  echo "" >> "$md_file" # Adds an extra newline for spacing
+  #echo "# $title" > "$md_file"
+  #echo "[![Watch the video](thumbnails/day${day}.png)]($url)" >> "$md_file"
+  #echo "" >> "$md_file" # Adds an extra newline for spacing
 
   # Execute the command for each video (Placeholder commands)
   yt --transcript "$url" | fabric --pattern create_summary --model mistral:instruct >> "$md_file"
-  yt --transcript "$url" | fabric --pattern create_summary --model llama3:latest >> "$md_file"
+  #yt --transcript "$url" | fabric --pattern create_summary --model llama3:latest >> "$md_file"
   
   echo "Processed $md_file"
 done
